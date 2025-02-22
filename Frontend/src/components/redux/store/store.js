74aee90307b16d1fa5d9c8559033b1f906b141f5
Faisal3347 <khanfaisal900413@gmail.com>
2025-02-22 14:49:16 +0530

    updated

--- Frontend/src/components/redux/store/store.js
+++ Frontend/src/components/redux/store/store.js
@@ -1,16 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit'
-<<<<<<< HEAD
-import { BaseApi } from '../baseQuery/BaseApi'
-
-export const store = configureStore({
-    reducer: {
-        [BaseApi.reducerPath]: BaseApi.reducer,
-    },
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware({
-        }).concat(BaseApi.middleware),
-});
-=======
 import counterReducer from '../slices/countersilces'
 
 export const store = configureStore({
@@ -18,4 +6,3 @@ export const store = configureStore({
         counter:counterReducer
     },
   })
->>>>>>> 74e274513bfd9382915811aeecf5fdd2f89f1e7e
